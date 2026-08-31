9d39d2bda9fb135f0dea23b6bb355d009a58a7c8
Dan van der Merwe <danieljmerwe@gmail.com>
2026-08-31 12:03:31 -0700

    chore(pr-review-eval): remove one-shot acceptance execution

--- examples/pr-review-eval/src/acceptance-259-main.ts
+++
@@ -1,6 +0,0 @@
-import { NodeRuntime, NodeServices } from "@effect/platform-node";
-import { Effect } from "effect";
-
-import { acceptance259 } from "./acceptance-259.ts";
-
-NodeRuntime.runMain(acceptance259.pipe(Effect.scoped, Effect.provide(NodeServices.layer)));
